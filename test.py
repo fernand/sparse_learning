a5490b12f0a3566f7f06ec199b9a4431e02fb194
Fernand Pajot <fernand@epigram.me>
2022-11-21 08:55:31 -0700

    .

--- test.py
+++ test.py
@@ -12,10 +12,10 @@ lib = ct.cdll.LoadLibrary('/home/fernand/sparse_learning/libsparse.so')
 lib.get_context.restype = ct.c_void_p
 context = ct.c_void_p(lib.get_context())
 
-A = torch.rand((16, 4096), dtype=torch.float16, device=torch.device(0))
+A = torch.rand((128*128, 4096), dtype=torch.float16, device=torch.device(0))
 mask = ampere.create_mask(A)
 B = torch.rand((10240, 4096), dtype=torch.float16, device=torch.device(0))
-C = torch.zeros((16, 10240), dtype=torch.float16, device=torch.device(0))
+C = torch.zeros((128*128, 10240), dtype=torch.float16, device=torch.device(0))
 
 lib.sparse_matmul(context, get_ptr(A), get_ptr(B), get_ptr(C), A.shape[0], A.shape[1], B.shape[0], B.shape[1])
 print((mask * A).matmul(B.T))
